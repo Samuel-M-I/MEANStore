d2d041d88c94b71bac2a43fc9421fd6c74602c15
Samuel Marroquín Isaza <samuelmarroquinisaza@gmail.com>
2026-03-07 18:24:47 -0500

    feat:se añadio el EndPoint de admin

---
+++ Backend/src/routes/admin.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const {getUsers} = require('../controllers/admin.controller');
+
+const router = express.Router();
+
+//Ruta solo para admin para obtener la lista de usuario y trabajadores
+router.get('/users',getUsers);
+//Cambia el rol de un usuario a trabajador o viceversa
+router.put('/users/:userId/role',changeUserRole);
+//Desactiva o activa un los privilegios que tiene un trbajador
+router.patch('/users/:userId/deactivate',changeUserRole);
+
+module.exports = router;
